4e47bf3a44a7b332bed610f37c99478b7ae7e909
Dimitris Laios <j13.laios@gmail.com>
2021-04-05 16:42:42 +0300

    commit on 'master'

--- src/model/filemanager.py
+++ src/model/filemanager.py
@@ -31,12 +31,19 @@ class FileManager:
         """
         return self.__createPlaylist(name)
 
-    def addTrackToPlaylist(self, filepath, name='All Tracks'):
+    def addTrackToPlaylist(self, filepath: str, name='All Tracks'):
         """
         Adds a track to the given playlist.
         
         """
         self.__addTrackToPlaylist(filepath, name)
+
+    def removeTrackFromPlaylist(self, filepath: str, name='All Tracks'):
+        """
+        Removes a track from the given playlist.
+        
+        """
+        self.__removeTrackFromPlaylist(filepath, name)
         
     def __loadTracks(self):
         return [f'{self.__trackPath}\\{file}' for file in os.listdir(self.__trackPath) if file.endswith('.mp3')]
@@ -67,8 +74,20 @@ class FileManager:
                     playlist = json.load(jsonFile)
                     if playlist['name'] == name:
                         playlist['tracks'].append(filepath)
-                with open(f'{self.__playlistPath}\\{file}', 'w', encoding='utf-8') as jsonFile:
-                    json.dump(playlist, jsonFile, indent=4)
-                return
+                        with open(f'{self.__playlistPath}\\{file}', 'w', encoding='utf-8') as jsonFile:
+                            json.dump(playlist, jsonFile, indent=4)
+                        return
 
-FileManager().addTrackToPlaylist('..\\music\\tracks\\shogeki.mp3', 'Yes Honey')
+    def __removeTrackFromPlaylist(self, filepath: str, name: str):
+        playlist = None
+        for file in os.listdir(self.__playlistPath):
+            if file.endswith('.json'):
+                with open(f'{self.__playlistPath}\\{file}', 'r', encoding='utf-8') as jsonFile:
+                    playlist = json.load(jsonFile)
+                    if playlist['name'] == name:
+                        playlist['tracks'].pop(playlist['tracks'].index(filepath))
+                        with open(f'{self.__playlistPath}\\{file}', 'w', encoding='utf-8') as jsonFile:
+                            json.dump(playlist, jsonFile, indent=4)
+                        return
+
+FileManager().removeTrackFromPlaylist('..\\music\\tracks\\shogeki.mp3', 'Yes Honey')
