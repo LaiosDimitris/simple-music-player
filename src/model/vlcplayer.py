d04cbd2a95faa054eb8f9d9a21f618bac7750642
Dimitris Laios <j13.laios@gmail.com>
2021-04-15 17:11:02 +0300

    commit

--- src/model/vlcplayer.py
+++ src/model/vlcplayer.py
@@ -1,17 +1,15 @@
 from . import FileManager
-from mutagen.mp3 import MP3
 import vlc
 
 vlc.MediaListPlayer
 
-class VlcPlayer:
+class VlcPlayer(vlc.Instance):
     """
-    VlcPlayer class that uses vlc to 
-    play individual tracks, playlists
-    and youtube videos.
+    VlcPlayer class that uses vlc to play 
+    individual tracks, playlists and youtube videos.
     
     """
     def __init__(self) -> None:
-        self.__vlc = vlc.Instance()
-        self.__mediaPlayer = self.__vlc.media_list_player_new()
+        super().__init__()
+        self.__mediaPlayer = self.media_player_new()
         
